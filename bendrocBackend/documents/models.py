d0599e6f5bf1ad4330226e81091e0faf664c1a9a
Phetso <phetso.nkosi@umuzi.org>
2026-05-30 18:24:07 +0200

    created models for payments

--- bendrocBackend/documents/models.py
+++ bendrocBackend/documents/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Document(models.Model):
 
     application = models.ForeignKey(
-        "applications.application",
+        "applications.Application",
         on_delete=models.CASCADE,
         related_name="documents"
     )
--- bendrocBackend/payments/models.py
+++ bendrocBackend/payments/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    STATUS_CHOICE = [
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("failed", "Failed"),
+    ]
+
+    application = models.OneToOneField(
+        "applications.Application",
+        on_delete=models.CASCADE
+    )
+
+
+    amount = models.DecimalField(
+        max_digits=10,
+          decimal_places=2
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICE,
+        default="pending"
+    )
+
+    reference = models.CharField(
+        max_length=255,
+        unique=True
+    )
+
+    paid_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.reference

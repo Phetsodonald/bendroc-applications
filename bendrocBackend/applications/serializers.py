88cfe4d297136c1c418b8912e0b1645af6de6915
Phetso <phetso.nkosi@umuzi.org>
2026-05-31 15:13:51 +0200

    created serializer for document model

--- bendrocBackend/applications/serializers.py
+++ bendrocBackend/applications/serializers.py
@@ -3,8 +3,20 @@ from .models import Application
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
+
+    application_type_name = serializers.CharField(
+        source="application_type.name",
+        read_only=True
+    )
+
     class Meta:
         model = Application
-        fields = "__all__"
-        read_only_fields = ("status", "created_at", "updated_at")
-        
+        fields = (
+            "id",
+            "user",
+            "application_type",
+            "application_type_name",
+            "status",
+            "created_at",
+            "updated_at",
+        )

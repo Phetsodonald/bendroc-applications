f8f1ad1625c508b3942795762ab6c063a9e8a7ea
Phetso <phetso.nkosi@umuzi.org>
2026-06-05 02:56:25 +0200

    create d view for application

--- bendrocBackend/applications/views.py
+++ bendrocBackend/applications/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Application
+from .serializers import ApplicationSerializer
+
+
+class ApplicationListCreateView(generics.ListCreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer

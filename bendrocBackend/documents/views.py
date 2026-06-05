2cc34b771d48ddaab35f44aee6bf860077c80e8f
Phetso <phetso.nkosi@umuzi.org>
2026-06-05 02:58:34 +0200

    created a view for document app

--- bendrocBackend/documents/views.py
+++ bendrocBackend/documents/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Document
+from .serializers import DocumentSerializer
+
+
+class DocumentListCreateView(generics.ListCreateAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
